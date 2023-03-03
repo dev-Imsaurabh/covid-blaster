64b1d79e5a0013b2c1624a9acf3716c273b98bc8
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-03-03 16:33:17 +0530

    home added

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,15 +1,27 @@
 import {Box} from "@chakra-ui/react"
 import { useRef ,useEffect} from "react";
 import { Game } from "../scripts/Game";
+import "./homepage.css";
+import "../styles/button.css"
 
 export default function Homepage({}){
     
-useEffect(()=>{
+// useEffect(()=>{
 
-    Game()
-},[])
-    return <Box mt={100}>
+//     Game()
+// },[])
+    return <Box className="home">
 
-
+       <img src="https://media.giphy.com/media/JmUd8L6SMdTrriXSEc/giphy.gif"/>
+       
+        <div className="homeButtons">
+            <p>Your room ID: 1234567</p>
+            <div>
+                <button className="button-85">CREATE A ROOM</button>
+                <button className="button-85">JOIN A ROOM</button>
+             </div>
+            
+        </div>
+       
     </Box>
 }
