a0919953014a56eadaa348ab41223c2dea231924
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-03-03 22:07:40 +0530

    added a route

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,6 +5,11 @@ import "./homepage.css";
 import "../styles/button.css"
 
 export default function Homepage({}){
+    const id = 123456
+
+    const handleClick = () => {
+         
+    }
     
 // useEffect(()=>{
 
@@ -15,10 +20,11 @@ export default function Homepage({}){
        <img src="https://media.giphy.com/media/JmUd8L6SMdTrriXSEc/giphy.gif"/>
        
         <div className="homeButtons">
-            <p>Your room ID: 1234567</p>
+            <p>Your room ID: {id}</p>
+            <img src="https://media3.giphy.com/media/dJezVlwfVulTykjRQj/giphy.gif?cid=ecf05e47bo36cie802msre03aopsq2y9zjr0d9s3bvb8wri1&rid=giphy.gif&ct=g"></img>
             <div>
-                <button className="button-85">CREATE A ROOM</button>
-                <button className="button-85">JOIN A ROOM</button>
+            <a href="https://my-app-rose-six.vercel.app/room"><button className="button-85" onClick={()=>handleClick()}>CREATE A ROOM</button></a>
+                <a href="https://my-app-rose-six.vercel.app/play"><button className="button-85" onClick={()=>handleClick()}>JOIN A ROOM</button></a>
              </div>
             
         </div>
--- src/routes/Allroutes.tsx
+++ src/routes/Allroutes.tsx
@@ -1,12 +1,12 @@
 import { Route,Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import { Room } from "../pages/room";
 
 export default function AllRoutes(){
 
     return <Routes>
 
         <Route path="/"  element={<Homepage />}></Route>
-
-
+        <Route path="/room"  element={<Room/>}></Route>
     </Routes>
 }
