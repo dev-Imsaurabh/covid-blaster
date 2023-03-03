cfc43445249a3d7879e4c308b6a6090bbd7c48c9
dev-Iamsaurabh <sau4478@gmail.com>
2023-03-03 12:34:30 +0530

    added game

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,15 @@
 import {Box} from "@chakra-ui/react"
+import { useRef ,useEffect} from "react";
+import { Game } from "../scripts/Game";
 
 export default function Homepage({}){
+    
+useEffect(()=>{
 
-
+    Game()
+},[])
     return <Box mt={100}>
 
-        Homepage
 
     </Box>
 }
