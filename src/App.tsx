7bc9f0b3c7d06e9679c444be346ae9e50e24c47d
dev-Iamsaurabh <sau4478@gmail.com>
2023-03-03 10:00:15 +0530

    repo initiated

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Box } from "@chakra-ui/react";
+import AllRoutes from "./routes/Allroutes";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <Box className="App">
+      <Navbar />
+      <AllRoutes />
+
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import {Box} from "@chakra-ui/react"
+
+export default function Homepage({}){
+
+
+    return <Box mt={100}>
+
+        Homepage
+
+    </Box>
+}
