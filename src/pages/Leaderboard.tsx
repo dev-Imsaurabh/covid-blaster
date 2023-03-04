8810977d211df49ff735b8f14b4535db519c6c42
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-03-04 22:27:04 +0530

    added leaderboard

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Container, Heading, Image, Table, Td, Text, Th, Tr } from 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/button.css"
+import {useEffect} from "react"
 
 export default function Leaderboard(){
 
@@ -11,48 +12,52 @@ export default function Leaderboard(){
     let score1 = localStorage.getItem("p1");
     let score2 = localStorage.getItem("p2")
 
-    // if(Number(score1) > Number(score2)){
-    //       setWinner("Player1 Wins")
-    // }else if(Number(score1) < Number(score2)){
-    //     setWinner("Player1 Wins")
-    // }else{
-    //     setWinner("Tie")
-    // }
+    
+
+    useEffect(()=>{
+        if(Number(score1) > Number(score2)){
+            setWinner("You Won")
+      }else if(Number(score1) < Number(score2)){
+          setWinner("Player2 Won")
+      }else{
+          setWinner("Tie")
+      }
+
+    },[])
 
     const handleClick = () => {
         nav("/")
     }
 
+
     return <Box>
-        <Heading fontSize="80px" mt="50px">GAME OVER</Heading>
+        <Heading fontSize="80px" mt="80px">GAME OVER</Heading>
         {/* <Heading>Leaderboard</Heading> */}
         <Container>
             <Image src=""/>
-        <Text fontSize={"40px"} mt="50px">{"Player1 Wins"}</Text>
+        {/* <Text fontSize={"40px"} mt="50px">{"Player1 Wins"}</Text> */}
         </Container>
+        <Heading>{winner}</Heading>
 
-        <Table width={"500px"} h="200px" border="1px solid white" margin={"auto"} mt="40px">
+        <Table width={"500px"} h="200px" border="1px solid white" color={"black"} fontWeight="700" fontSize={"25px"} margin={"auto"} mt="40px">
             <Tr>
-                <Td>Name</Td>
-                <Td>Score</Td>
-                <Td>Player ID</Td>
+                <Th>Name</Th>
+                <Th>Score</Th>
             </Tr>
             <Tr>
-                <Td>Player1</Td>
+                <Td>You</Td>
                 <Td>{score1}</Td>
-                <Td>1234</Td>
             </Tr>
 
             <Tr>
                 <Td>Player2</Td>
                 <Td>{score2}</Td>
-                <Td>12345</Td>
             </Tr>
            
 
         </Table>
 
-        <Button className="button-85" mt={"80px"} onClick={()=>handleClick()}>Go to home</Button>
+        <Button className="button-85" mt={"80px"} mb="110px" fontSize={"25px"} p="30px 20px" onClick={()=>handleClick()}>Go to home</Button>
     
     </Box>
 }
